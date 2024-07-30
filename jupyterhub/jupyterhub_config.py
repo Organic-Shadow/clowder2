ed69cf5db3e54f241129de6a6279c57976117656
Vismayak Mohanarajan <mohanar2@illinois.edu>
2024-07-30 10:32:37 -0500

    test

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -107,6 +107,11 @@ else:
 
 c.JupyterHub.cookie_secret = os.getenv("JUPYTERHUB_CRYPT_KEY")
 
+# Pass user cookie as environment variable to spawned containers
+c.DockerSpawner.environment = {
+    "JUPYTERHUB_USER3": "{username}",
+}
+
 # Allowed admins
 admin = os.environ.get("JUPYTERHUB_ADMIN")
 if admin:
